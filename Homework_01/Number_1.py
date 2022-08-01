fb9d639caf4482d3f1b4baddb8f51c7285a19ed2
DianovaAA <Dianova05@gmail.com>
2022-08-01 23:15:16 +0300

    ДЗ №1

---
+++ Homework_01/Number_1.py
@@ -0,0 +1,8 @@
+# This Python file uses the following encoding: utf-8
+# Написать цикл для выведения на экран каждой буквы своего ФИО.
+
+fio = 'Dianova Alena Alekseevna'
+
+for x in fio:
+    if x != " ":
+        print(x)
---
+++ Homework_01/Number_2.py
@@ -0,0 +1,13 @@
+# This Python file uses the following encoding: utf-8
+# Написать функцию для нахождения одинаковых элементов в двух списках.
+
+def comp_elements(a: list, b: list):
+    for elemA in a:
+        for elemB in b:
+            if elemA == elemB:
+                print(elemA)
+
+list_1 = input("Введите первый список: ").split()
+list_2 = input("Введите второй список: ").split()
+
+comp_elements(list_1, list_2)
